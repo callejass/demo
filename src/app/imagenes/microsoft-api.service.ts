5c10a84bad0059c01ce157a4e83d19b1e4603769
sergio.callejas <sergio.callejas@grupoica.com>
2019-01-15 17:40:46 +0100

    Añadida llamada a mas servicios

--- src/app/imagenes/microsoft-api.service.ts
+++ src/app/imagenes/microsoft-api.service.ts
@@ -6,25 +6,44 @@ import { catchError, map } from 'rxjs/operators';
   providedIn: 'root'
 })
 export class MicrosoftApiService {
+  private _apiconfig: any = {
+      urlbase: 'https://westeurope.api.cognitive.microsoft.com',
+      key: 'bdb1e60308f245abbcaa80617360cc91'
+  };
+  // private _apikey = 'bdb1e60308f245abbcaa80617360cc91';
+  // private _url = '';
 
-  private _url = '';
+
   constructor(private httpClient: HttpClient) { }
 
   getFaces(imageDataUrl): Observable<any> {
-    const apikey = 'bdb1e60308f245abbcaa80617360cc91';
+    // const apikey = 'bdb1e60308f245abbcaa80617360cc91';
     // tslint:disable-next-line:max-line-length
-    const url = 'https://westeurope.api.cognitive.microsoft.com/face/v1.0/detect?returnFaceAttributes=age,gender,headPose,smile,facialHair,glasses,emotion,hair,makeup,occlusion,accessories,blur,exposure,noise';
+    // const url = 'https://westeurope.api.cognitive.microsoft.com/face/v1.0/detect?returnFaceAttributes=age,gender,headPose,smile,facialHair,glasses,emotion,hair,makeup,occlusion,accessories,blur,exposure,noise';
+
+    const url = `${this._apiconfig.urlbase}/face/v1.0/detect?returnFaceAttributes=age,gender,headPose,smile,facialHair,glasses,emotion,hair,makeup,occlusion,accessories,blur,exposure,noise`;
+    return this.sendRequest(imageDataUrl, url);
+  }
+
+  getInfo(imageDataUrl): Observable<any> {
+    const url = `${this._apiconfig.urlbase}//v1.0/detect?returnFaceAttributes=age,gender,headPose,smile,facialHair,glasses,emotion,hair,makeup,occlusion,accessories,blur,exposure,noise`;
+    return this.sendRequest(imageDataUrl, url);
+  }
+
+  private sendRequest(imageDataUrl: string, apiUrl: string) {
+
     let headers: HttpHeaders = new HttpHeaders();
-    headers = headers.set('Ocp-Apim-Subscription-Key', apikey);
+    headers = headers.set('Ocp-Apim-Subscription-Key', this._apiconfig.key);
     headers = headers.set('Content-Type', 'application/octet-stream');
 
-    return this.httpClient.post<any>(url, this.blobFromDataUrl(imageDataUrl), {headers: headers})
+    return this.httpClient.post<any>(apiUrl, this.blobFromDataUrl(imageDataUrl), {headers: headers})
     .pipe(
       map(response => {
         return response;
       })
     );
   }
+
 
 
   private blobFromDataUrl(dataURL) {
