1b3e7b5d4ff54903fae60851b6b1495727c95716
Sergio <callejass@gmail.com>
2019-01-17 22:28:10 +0100

    aaa

---
+++ src/app/shared/custom-modal-footer/custom-modal-footer.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'scg-custom-modal-footer',
+  templateUrl: './custom-modal-footer.component.html',
+  styleUrls: ['./custom-modal-footer.component.css']
+})
+export class CustomModalFooterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CustomChartBarComponent } from './custom-chart-bar/custom-chart-bar.component';
 import { CustomModalHeaderComponent } from './custom-modal-header/custom-modal-header.component';
+import { CustomModalFooterComponent } from './custom-modal-footer/custom-modal-footer.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  exports: [CustomModalHeaderComponent],
-  declarations: [CustomChartBarComponent, CustomModalHeaderComponent]
+  exports: [CustomModalHeaderComponent, CustomModalFooterComponent],
+  declarations: [CustomChartBarComponent, CustomModalHeaderComponent, CustomModalFooterComponent]
 })
 export class SharedModule { }
